dfa0f3632b1c45f66f1dae9e2f44121d715837b2
Michel Grootjans <michel.grootjans@gmail.com>
2015-04-23 13:41:57 +0100

    go up 1 floor

--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -4,12 +4,23 @@ class Elevator
   def initialize
     @floor = 0
   end
+
+  def go_up
+    @floor += 1
+  end
 end
 
 describe Elevator do
   describe "by default" do
     it "is at ground level" do
       subject.floor.should == 0
+    end
+  end
+
+  describe "going up 1 floor" do
+    it "is at ground level" do
+      subject.go_up
+      subject.floor.should == 1
     end
   end
 end
